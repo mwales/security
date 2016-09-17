3538566cdbc40d2049a932d9dc75f900a3bc14dd
Michael Wales <mwales3@gmail.com>
2016-09-17 02:41:43 -0400

    Cleaned up python code a bit

--- ctf/TokyoWesterns2016/reverse_box/generate_ciphers.py
+++ ctf/TokyoWesterns2016/reverse_box/generate_ciphers.py
@@ -10,14 +10,14 @@ if len(sys.argv) != 2:
    sys.exit()
 
 cipherTable = ""
+
+# You can't just use all printable ASCII, because a lot of non-alphanumeric characters will piss of the shell.
 #for printableChar in range(20,127):
+
 for printableChar in range(48,57) + range(65,90) + range(97,122) + [123, 125, 45, 95]:
-   #if printableChar in [96, 42, 44, 63, 59, 124, 33, 60, 61, 62, 92]: #[96, 42, 44, 63, 59, 124, 33, 60, 61, 62]:
-      #continue
    cipherTable += chr(printableChar)
 
 print "ASCII Table =" + cipherTable
-
 
 for cipherIndex in range(1, 256):
    print "Cipher Index", cipherIndex
@@ -26,15 +26,11 @@ for cipherIndex in range(1, 256):
 
    #print ("Patch code = " + patchCode)
 
-   #call("./patch32", "reverse_box.hacked", "0x80485ac", "0xe", patchCode)
-   #os.system("./patch32 reverse_box.hacked 0x80485ac 0xe " + patchCode)
    print("Patching: ./patch32 reverse_box.hacked 0x80485ac 0xe " + patchCode)
    patchProcessOutput = subprocess.Popen("./patch32 reverse_box.hacked 0x80485ac 0xe " +  patchCode, shell=True, stdout=subprocess.PIPE).stdout.read()
    
    #print "Patch process output:  " + patchProcessOutput
 
-   #call("./reverse_box.hacked", cipherTable)
-   #os.system("./reverse_box.hacked " + cipherTable + " >> table.txt")
 
    decipherTable = subprocess.Popen("./reverse_box.hacked \"" + cipherTable + "\"", shell=True, stdout=subprocess.PIPE).stdout.read()
 
