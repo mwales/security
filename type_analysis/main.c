ad34b088bf1ce51df79bc8df5c2a1c38db2912af
Michael Wales <mwales3@gmail.com>
2018-12-29 15:27:55 -0500

    Added hex to offset output of the stat structure

--- type_analysis/main.c
+++ type_analysis/main.c
@@ -8,23 +8,23 @@ int main(int argc, char** argv)
 {
    struct stat s;
 
-   printf("Size of struct stat = %d\n", sizeof(struct stat));
+   printf("Size of struct stat = %d = 0x%x\n", sizeof(struct stat), sizeof(struct stat));
 
    int baseAddr = (int) &s;
    int offset_st_ino = (int) &s.st_ino - baseAddr;
-   printf("  Offset fo st_ino = %d\n", offset_st_ino);
+   printf("  Offset fo st_ino = %d = 0x%x\n", offset_st_ino, offset_st_ino);
 
    int offset_st_mode = (int) &s.st_mode - baseAddr;
-   printf("  Offset of st_mode = %d\n", offset_st_mode);
+   printf("  Offset of st_mode = %d = 0x%x\n", offset_st_mode, offset_st_mode);
 
    int offset_st_uid = (int) &s.st_uid - baseAddr;
-   printf("  Offset of st_uid = %d\n", offset_st_uid);
+   printf("  Offset of st_uid = %d = 0x%x\n", offset_st_uid, offset_st_uid);
 
    int offset_st_gid = (int) &s.st_gid - baseAddr;
-   printf("  Offset of st_gid = %d\n", offset_st_gid);
+   printf("  Offset of st_gid = %d = 0x%x\n", offset_st_gid, offset_st_gid);
 
    int offset_st_size = (int) &s.st_size - baseAddr;
-   printf("  Offset of st_size = %d\n", offset_st_size);
+   printf("  Offset of st_size = %d = 0x%x\n", offset_st_size, offset_st_size);
 
    //int offset_st_attr = (int) &s.st_attr - baseAddr;
    //printf("  Offset of st_attr = %d\n", offset_st_attr);
